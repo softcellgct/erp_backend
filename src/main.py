e129149357c3a2e89d8cc7a0ba13f2529e1c291f
prithiviraj2804 <prithivi2804raj@gmail.com>
2026-04-29 08:02:16 +0000

    feat: remove specific CORS origins from middleware configuration

--- src/main.py
+++ src/main.py
@@ -38,14 +38,6 @@ app.add_middleware(AuthMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "https://erp.thamizhoviyaa.in",
-        
-    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
